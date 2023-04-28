51217b859e5dfec1d64293760fbdaa0e6484b372
iamstan13y <iamstan13y@gmail.com>
2023-04-29 00:35:56 +0200

    Repository Pattern & UoW Config.

---
+++ Sources/Analysis.API/Models/Repository/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using StockAnalysis.API.Models.Data;
+using StockAnalysis.API.Models.Repository.IRepository;
+
+namespace StockAnalysis.Models.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void SaveChanges() => _context.SaveChanges();
+    }
+}
