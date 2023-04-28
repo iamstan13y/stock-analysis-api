85100f48e61c60d0d38621cad63751671ea10e24
iamstan13y <iamstan13y@gmail.com>
2023-04-29 00:49:48 +0200

    Added Company Controller.

---
+++ Sources/Analysis.API/Controllers/CompanyController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockAnalysis.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+    }
+}
