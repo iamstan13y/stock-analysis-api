39737393989f616bcc52c5a3edd59aad2be69b1a
iamstan13y <iamstan13y@gmail.com>
2023-04-28 23:25:50 +0200

    Code Clean-up.

--- Sources/UserManagement.API/Models/Data/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserManagement.API.Models.Data
-{
-    public class LoginRequest
-    {
-        public string? Email { get; set; }
-        public string? Password { get; set; }
-    }
-}
