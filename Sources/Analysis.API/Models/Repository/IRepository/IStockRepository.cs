1883c18647b9c0bc9432cf2b6f135cf1c84cf65d
iamstan13y <iamstan13y@gmail.com>
2023-04-29 01:50:04 +0200

    Stock Repository & UoW DI.

---
+++ Sources/Analysis.API/Models/Repository/IRepository/IStockRepository.cs
@@ -0,0 +1,9 @@
+﻿using StockAnalysis.API.Models.Data;
+using StockAnalysis.Models.Repository.IRepository;
+
+namespace StockAnalysis.API.Models.Repository.IRepository
+{
+    public interface IStockRepository : IRepository<Stock>
+    {
+    }
+}
