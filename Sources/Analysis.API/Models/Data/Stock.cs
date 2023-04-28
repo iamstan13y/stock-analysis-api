fde1c0d07131566438ab06cadf61eb3a2fd2319f
iamstan13y <iamstan13y@gmail.com>
2023-04-29 01:45:05 +0200

    Stock Model & DB Migrations.

---
+++ Sources/Analysis.API/Models/Data/Stock.cs
@@ -0,0 +1,11 @@
+﻿namespace StockAnalysis.API.Models.Data
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        public double ClosingPrice { get; set; }
+        public DateTime ClosingDate { get; set; } = DateTime.Now;
+        public Company? Company { get; set;}
+    }
+}
