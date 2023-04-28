03a692e9b6e5b936075534bcb7b3b9bfaa8756d4
iamstan13y <iamstan13y@gmail.com>
2023-04-29 00:35:43 +0200

    Repository Pattern & UoW Config.

---
+++ Sources/Analysis.API/Models/Repository/IRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using StockAnalysis.API.Models.Local;
+using StockAnalysis.Utility;
+using System.Linq.Expressions;
+
+namespace StockAnalysis.Models.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<Result<T>> FindAsync(int id);
+        Task<Result<T>> GetFirstOrDefaultAsync(Expression<Func<T, bool>> filter);
+        Task<Result<IEnumerable<T>>> GetAllAsync();
+        Task<Result<Pageable<T>>> GetAllPagedAsync(Pagination pagination);
+        Task<Result<T>> AddAsync(T entity);
+        Task<Result<bool>> DeleteAsync(T entity);
+        Task<Result<bool>> DeleteAsync(int id);
+        Task<Result<T>> UpdateAsync(T entity);
+    }
+}
