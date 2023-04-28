17e75764ef1bdf1f324b05b1cfa90678effdb5da
iamstan13y <iamstan13y@gmail.com>
2023-04-29 01:04:57 +0200

    Minor fixes.

--- Sources/Analysis.API/Program.cs
+++ Sources/Analysis.API/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using StockAnalysis.API.Models.Data;
 using StockAnalysis.API.Models.Repository.IRepository;
+using StockAnalysis.Models.Repository;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<IUnitOfWork, IUnitOfWork>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 builder.Services.AddCors(options =>
 {
