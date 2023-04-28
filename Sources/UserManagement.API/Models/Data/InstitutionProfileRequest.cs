c28e1b96ba83acc924947884c0a1ccaf8d667882
iamstan13y <iamstan13y@gmail.com>
2023-04-28 23:25:45 +0200

    Code Clean-up.

--- Sources/UserManagement.API/Models/Data/InstitutionProfileRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UserManagement.API.Models.Data
-{
-    public class InstitutionProfileRequest
-    {
-        public int AccountId { get; set; }
-        public string? InstitutionName { get; set; }
-        public string? Address { get; set; }
-    }
-}
