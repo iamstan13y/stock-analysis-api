da6c91e95ac50637b386d8b590970bbaa1ac3ce9
iamstan13y <iamstan13y@gmail.com>
2023-04-28 23:25:10 +0200

    Code Clean-up.

--- Sources/UserManagement.API/Models/Data/EmailRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UserManagement.API.Models.Data
-{
-    public class EmailRequest
-    {
-        public string? To { get; set; }
-        public string? Subject { get; set; }
-        public string? Body { get; set; }
-    }
-}
