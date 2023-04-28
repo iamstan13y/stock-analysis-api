f2d2a921397b1b2ff0a0def997cd90673026c157
iamstan13y <iamstan13y@gmail.com>
2023-04-29 00:48:33 +0200

    Company Repository Pattern.

---
+++ Sources/Analysis.API/Models/Repository/CompanyRepository.cs
@@ -0,0 +1,12 @@
+﻿using StockAnalysis.API.Models.Data;
+using StockAnalysis.API.Models.Repository.IRepository;
+
+namespace StockAnalysis.API.Models.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        public CompanyRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
