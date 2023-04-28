1a1972d2ae42b7bd64fc10286dd337f148c1422b
iamstan13y <iamstan13y@gmail.com>
2023-04-28 23:24:53 +0200

    Code Clean-up.

--- Sources/UserManagement.API/Models/Data/ChangePasswordRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UserManagement.API.Models.Data
-{
-    public class ChangePasswordRequest
-    {
-        public int UserId { get; set; }
-        public string? OldPassword { get; set; }
-        public string? NewPassword { get; set; }
-    }
-}
