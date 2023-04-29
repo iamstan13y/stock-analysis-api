e6040a0735e5df3e01cd49df578dfdc78d9c0f54
iamstan13y <iamstan13y@gmail.com>
2023-04-29 12:50:20 +0200

    Get All Stocks Implementation.

--- Sources/Analysis.API/Models/Repository/StockRepository.cs
+++ Sources/Analysis.API/Models/Repository/StockRepository.cs
@@ -1,4 +1,6 @@
-﻿using StockAnalysis.API.Models.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using StockAnalysis.API.Models.Data;
+using StockAnalysis.API.Models.Local;
 using StockAnalysis.API.Models.Repository.IRepository;
 
 namespace StockAnalysis.API.Models.Repository
@@ -8,5 +10,9 @@ namespace StockAnalysis.API.Models.Repository
         public StockRepository(AppDbContext context) : base(context)
         {
         }
-    }
+
+        public async new Task<Result<IEnumerable<Stock>>> GetAllAsync() =>
+            new Result<IEnumerable<Stock>>(await _dbSet
+                .Include(x => x.Company)
+                .ToListAsync());
 }
