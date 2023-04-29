f7b1129a24805fb90e40f300d941b61473e0f102
iamstan13y <iamstan13y@gmail.com>
2023-04-29 23:41:22 +0200

    Cron Job To Update Stocks Daily.

---
+++ Sources/Analysis.API/Jobs/StockJob.cs
@@ -0,0 +1,35 @@
+﻿using Quartz;
+using StockAnalysis.API.Models.Data;
+
+namespace StockAnalysis.API.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class StockJob : IJob
+    {
+        private readonly AppDbContext _context;
+
+        public StockJob(AppDbContext context) => _context = context;
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            var latestStocks = _context.Stocks!
+                .OrderByDescending(x => x.ClosingDate)
+                .Take(10)
+                .Select(stock => new Stock
+                {
+                    CompanyId = stock.CompanyId,
+                    ClosingPrice = PrognosticateClosingPrice(stock.ClosingPrice),
+                    ClosingDate = DateTime.Now
+                })
+                .ToList();
+
+            _context.Stocks!.AddRange(latestStocks);
+            _context.SaveChanges();
+
+            return Task.CompletedTask;
+        }
+
+        private static double PrognosticateClosingPrice(double closingPrice) => 
+            (closingPrice * 0.9) + (new Random().NextDouble() * ((closingPrice * 1.1) - (closingPrice * 0.9)));
+    }
+}
