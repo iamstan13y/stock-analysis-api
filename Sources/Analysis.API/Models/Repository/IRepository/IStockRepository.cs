d48e409ca2002f5bce0925a2c4567178aa1429b3
iamstan13y <iamstan13y@gmail.com>
2023-04-29 13:09:24 +0200

    Get Stocks By Company Id Implementation.

--- Sources/Analysis.API/Models/Repository/IRepository/IStockRepository.cs
+++ Sources/Analysis.API/Models/Repository/IRepository/IStockRepository.cs
@@ -1,9 +1,11 @@
 ﻿using StockAnalysis.API.Models.Data;
+using StockAnalysis.API.Models.Local;
 using StockAnalysis.Models.Repository.IRepository;
 
 namespace StockAnalysis.API.Models.Repository.IRepository
 {
     public interface IStockRepository : IRepository<Stock>
     {
+        Task<Result<IEnumerable<Stock>>> GetByCompanyIdAsync(int companyId);
     }
 }
