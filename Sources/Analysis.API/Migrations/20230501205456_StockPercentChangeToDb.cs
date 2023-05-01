3f51ed52aa24e260f8778230faf6de93f0edb195
iamstan13y <iamstan13y@gmail.com>
2023-05-01 23:01:52 +0200

    Stock Percentage Change & DB Migrations.

---
+++ Sources/Analysis.API/Migrations/20230501205456_StockPercentChangeToDb.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockAnalysis.API.Migrations
+{
+    public partial class StockPercentChangeToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "PercentageChange",
+                table: "Stocks",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PercentageChange",
+                table: "Stocks");
+        }
+    }
+}
