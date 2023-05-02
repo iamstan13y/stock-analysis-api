916815ebd07ef65df40a85cbf8c735244d7167f0
iamstan13y <iamstan13y@gmail.com>
2023-05-02 15:14:58 +0200

    Stock Verdict.

---
+++ Sources/Analysis.API/Migrations/20230502131409_StockVerdictToDb.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockAnalysis.API.Migrations
+{
+    public partial class StockVerdictToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Verdict",
+                table: "Stocks",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Verdict",
+                table: "Stocks");
+        }
+    }
+}
